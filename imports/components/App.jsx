fc2c802cd021ab5cb09e404f035e676f0c305462
Afzal Hossain <afzal.csedu@gmail.com>
2016-09-06 15:53:57 +0600

    active path fix

--- imports/components/App.jsx
+++ imports/components/App.jsx
@@ -14,7 +14,7 @@ export default class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-4 col-md-3 hidden-xs">
-                        <LeftSidebar />
+                        <LeftSidebar path={this.props.location.pathname} />
                     </div>
                     <div className="col-sm-8 col-md-9 col-xs-12">
                         <ReactCSSTransitionGroup
--- imports/components/LeftSidebar.jsx
+++ imports/components/LeftSidebar.jsx
@@ -18,7 +18,7 @@ export default class LeftSidebar extends React.Component {
                     <div className="col-xs-12">
                         <nav className="left-menu">
                             <ul>
-                                <li><Link to="/" activeClassName="active">Home<i className="fa fa-home"></i></Link></li>
+                                <li><Link to="/" activeClassName={(this.props.path === '/')?'active':''}>Home<i className="fa fa-home"></i></Link></li>
                                 <li><Link to="/products" activeClassName="active">Products<i className="fa fa-home"></i></Link></li>
                                 <li><Link to="/services" activeClassName="active">Services<i className="fa fa-home"></i></Link></li>
                                 <li><Link to="/support" activeClassName="active">Support/Contact<i className="fa fa-home"></i></Link></li>
